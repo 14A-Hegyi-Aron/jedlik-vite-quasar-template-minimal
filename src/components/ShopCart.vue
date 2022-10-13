086cdc1693d09170070a44ae4bdd3c273c1584c7
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2022-10-13 11:35:51 +0200

    2022.10.13

---
+++ src/components/ShopCart.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  import { IShopItem } from "src/types";
+  import { PropType } from "vue";
+
+  const props = defineProps({
+    cart: {
+      type: Object as PropType<IShopItem[]>,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  });
+
+  const emmit = defineEmits(["empty-cart"]);
+
+  function emptyCart() {
+    emmit("empty-cart");
+  }
+</script>
+
+<template>
+  <q-card class="bg-info">
+    <q-card-actions>
+      <p>Cart component: {{ cart.length }} items</p>
+      <q-space />
+      <q-btn class="negative" color="primary" no-caps @click="emptyCart()">Empty cart</q-btn>
+    </q-card-actions>
+    <q-card-section>
+      <div v-for="item in cart" :key="item.id">
+        <p>{{ item.name }} - {{ item.price }} Ft</p>
+      </div>
+      <p>Total: {{ total }} Ft</p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/StartPageView.vue
+++ src/views/StartPageView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { IShopItem } from "src/types";
   import ShopItem from "src/components/ShopItem.vue";
+  import ShopCart from "src/components/ShopCart.vue";
 
   const items: Array<IShopItem> = reactive([
     { id: 201, name: "Alma", price: 12, imageSrc: "src/assets/alma.jpg" },
@@ -14,16 +15,26 @@
     { id: 209, name: "Szamóca", price: 108, imageSrc: "src/assets/szamóca.jpg" },
   ]);
 
-  function UpdateCart(e: IShopItem) {
-    console.log(e);
+  let cart: Array<IShopItem> = reactive([]);
+  let total = ref(0);
+
+  function updateCart(e: IShopItem) {
+    cart.push(e);
+    total.value += e.price;
+  }
+
+  function emptyCart() {
+    cart.splice(0);
+    total.value = 0;
   }
 </script>
 
 <template>
   <q-page>
+    <shop-cart :cart="cart" :total="total" @empty-cart="emptyCart()" />
     <div class="row">
       <div v-for="item in items" :key="item.id" class="col-xs-12 col-sm6 col-md-4 col-lg-3">
-        <shop-item :item="item" @update-cart="UpdateCart" />
+        <shop-item :item="item" style="mix-blend-mode: luminosity" @update-cart="updateCart" />
       </div>
     </div>
   </q-page>
